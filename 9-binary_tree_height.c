26058cc5e9a0c740ab0384bdb3ba255ce9ad03d3
adamclear <3589@holbertonschool.com>
2022-08-04 16:58:19 +0000

    updated with conditionals

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,10 +11,12 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t right = 0;
 
 	if (!tree)
-		return (-1);
+		return (0);
 
-	left = binary_tree_height(tree->left) + 1;
-	right = binary_tree_height(tree->right) + 1;
+	if (tree->left)
+		left = binary_tree_height(tree->left) + 1;
+	if (tree->right)
+		right = binary_tree_height(tree->right) + 1;
 
 	return (left > right ? left : right);
 }
